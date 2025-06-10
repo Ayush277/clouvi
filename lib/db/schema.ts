7dd897b784aaf353fd04ef3b4b8c86a61af62a19
Ayush Kumar <prince908ayush@gmail.com>
2025-06-10 10:15:07 +0530

    updated Schema

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,2 +1,40 @@
-import {pgTable, text, uuid, integer, boolean} from  "drizzle-orm/pg-core";
-import { Relations } from "drizzle-orm";
+import {pgTable, text, uuid, integer, boolean, timestamp} from  "drizzle-orm/pg-core";
+import { is, Relations } from "drizzle-orm";
+import path from "path";
+import { fileURLToPath } from "url";
+import { use } from "react";
+
+export const files = pgTable("files", {
+    id : uuid("id").defaultRandom().primaryKey(),
+
+    //basic file and folder information
+    name : text("name").notNull(),//it show that name cannot be null,
+    path : text("path").notNull(), //document/project/file.txt
+    size : integer("size").notNull(), //it show that size cannot be null
+    type : text("type").notNull(), //it show that type cannot be null "FOLDER"
+
+
+    //Storing Information for the user and the data cause we are making a self relation thing in it.
+    fileUrl: text("fileUrl").notNull(), //url accessing the file
+    thumbnailUrl: text("thumbnailUrl"), //url accessing the thumbnail of the file
+
+
+    //Owner of the file
+    userId: text("userId").notNull(),
+    parentId: uuid("parentId"), //it show that parentId can be null, it is used to make a self relation thing
+
+
+    //file and folder flags (where we define marked star , bookmark and trashed)
+    isfolder: boolean("isfolder").default(false).notNull(), //it show that isfolder cannot be null, it is used to make a self relation thing
+    isStarred: boolean("isStarred").default(false).notNull(), //it show that isStarred cannot be null, it is used to make a self relation thing
+    isBookmarked: boolean("isBookmarked").default(false).notNull(), //it show that isBookmarked cannot be null, it is used to make a self relation thing
+    istrashed: boolean("istrashed").default(false).notNull(), //it show that istrashed cannot be null, it is used to make a self relation thing
+
+    //Timestamps for the file
+    createdAt: timestamp("createdAt").defaultNow().notNull(), //it show that createdAt cannot be null, it is used to make a self relation thing
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(), //it show that updatedAt cannot be null, it is used to make a self relation thing
+
+
+
+
+})
