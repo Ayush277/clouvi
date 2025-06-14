d5680d7a844fb81eec7bf884a7def037fcce5f8f
Ayush Kumar <prince908ayush@gmail.com>
2025-06-14 14:40:19 +0530

    backend

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -8,9 +8,13 @@ import {z} from "zod";
 import { signupSchema } from "@/schemas/signUpSchema";
 import { use } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Result } from "pg";
 
 export default function SignUpForm() {
     const [verifying, setVerifying] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [setVerificationCode] = useState ("")
+    const [authError, setAuthError] = useState<string | null>(null);
 
     const { signUp, isLoaded, setActive } = useSignUp();
 
@@ -31,9 +35,46 @@ export default function SignUpForm() {
     const onSubmit = async (data: z.infer<typeof signupSchema>) => {
         if (!isLoaded) 
             return;
-    }
+        setIsSubmitting(true);
+        setAuthError(null);
 
-    const handleVerificationSubmit = async () => {}
+        try {
+            await signUp.create({
+                emailAddress: data.email,
+                password: data.password,
+            });
+            await signUp.prepareEmailAddressVerification({
+                strategy: "email_code",
+            });
+            setVerifying(true);
+        } catch (error: any) {
+            console.error("Sign up error:", error);
+            setAuthError(
+                error.errors?.[0]?.message || "An unexpected error occurred. Please try again."
+            )
+        }
+        finally{
+            setIsSubmitting(false);
+        }    };
+
+    const handleVerificationSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!isLoaded || !signUp) 
+            return
+         setIsSubmitting(true);
+        setAuthError(null);
+
+        try {
+            const result = await signUp.attemptEmailAddressVerification({
+                code: setVerificationCode,
+            });
+            if(result.status === "complete") {
+                await setActive({ session: result.createdSessionId });
+            }
+        } catch (error) {
+            
+        }
+    };
 
     if (verifying) {
         return(
